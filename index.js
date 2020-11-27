ee9e932da9cd189d3aec776464ad9e2ce03c9100
Marcela <lagil.marce@gmail.com>
2020-11-26 22:50:48 -0500

    adding dates of visits

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
 const app = express();
+const port = 3000;
 
-let counter = 0;
+// settings
+app.engine('handlebars', hbs({
+  defaultLayout: 'main', 
+  layoutsDir: path.join(__dirname, 'views/layouts')}));
+app.set('view engine', 'handlebars');
 
+
+//Visits
 let allNows = [];
 
+//Routing
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-    counter++;
+    res.render('index', { title: 'Home' });
     //Getting date
     const now = new Date();
-    allNows.push(now)
+    allNows.push(now);
+    console.log(allNows)
 })
+
+//for (let i = 0; i <= allNows.length; i++)
+
 
 app.get('/stats', (req, res) => {
-  res.send(counter + " visitors");
+  res.render('stats', {
+    total: allNows.length,
+    allNows: allNows,
+    title: 'Stats'
+  })
+  //res.send(allNows.length + " visitors");
 })
 
-const port = 3000;
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
